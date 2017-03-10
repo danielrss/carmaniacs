1fc024c41bf1cb981ed95312366c5353989308d9
Daniel Isov <danielisov96@gmail.com>
2017-03-10 22:00:30 +0200

    added unit test project for the web client

---
+++ CarManiacs/UnitTests/CarManiacs.WebClient.UnitTests/TestTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CarManiacs.WebClient.UnitTests
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
