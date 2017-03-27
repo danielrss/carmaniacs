721352f691cb29cd7256580c1fe191dfb999caf3
Daniel Isov <danielisov96@gmail.com>
2017-03-27 10:45:00 +0300

    added unit tests for ProjectsController

---
+++ CarManiacs/UnitTests/CarManiacs.WebClient.UnitTests/ProjectsController/Index_Should.cs
@@ -0,0 +1,31 @@
+﻿using CarManiacs.Business.Common;
+using CarManiacs.Business.Services.Contracts;
+using CarManiacs.WebClient.Models;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TestStack.FluentMVCTesting;
+
+namespace CarManiacs.WebClient.UnitTests.ProjectsController
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void ReturnDefaultViewAndCallProjectService()
+        {
+            // Arrange
+            var regularUserServiceMock = new Mock<IRegularUserService>();
+            var projectServiceMock = new Mock<IProjectService>();
+            var projectsController = new WebClient.Controllers.ProjectsController(projectServiceMock.Object, regularUserServiceMock.Object);
+
+            //Act & Assert
+            projectsController
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView()
+                .WithModel<IEnumerable<ProjectShortViewModel>>();
+
+            projectServiceMock.Verify(m => m.Get(0, Constants.EntitiesPerPage), Times.Once);
+        }
+    }
+}
---
+++ CarManiacs/UnitTests/CarManiacs.WebClient.UnitTests/ProjectsController/LoadPage_Should.cs
@@ -0,0 +1,29 @@
+﻿using CarManiacs.Business.Common;
+using CarManiacs.Business.Models.Projects;
+using CarManiacs.Business.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace CarManiacs.WebClient.UnitTests.ProjectsController
+{
+    [TestFixture]
+    public class LoadPage_Should
+    {
+        public void ReturnJsonResult_WhenProjectsCollectionIsEmpty()
+        {
+            // Arrange
+            var regularUserServiceMock = new Mock<IRegularUserService>();
+            var projectServiceMock = new Mock<IProjectService>();
+            var projectsController = new WebClient.Controllers.ProjectsController(projectServiceMock.Object, regularUserServiceMock.Object);
+            var projectMock = new Mock<Project>();
+
+            //Act & Assert
+            projectsController
+                .WithCallTo(c => c.LoadPage(3))
+                .ShouldReturnJson();
+
+            projectServiceMock.Verify(m => m.Get(3, Constants.EntitiesPerPage), Times.Once);
+        }
+    }
+}
